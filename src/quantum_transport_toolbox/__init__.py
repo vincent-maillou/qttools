d28ce46f7b8e26e667b31218b7905c6b7194075f
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-04-15 18:50:27 +0200

    corrected typos

---
+++ src/quantum_transport_toolbox/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2023-2024 ETH Zurich and Quantum Transport Toolbox authors. All rights reserved.
+from quantum_transport_toolbox.__about__ import __version__
+
+__all__ = ["__version__"]
---
+++ src/quantum_transport_toolbox/open_boundary_conditions_solvers/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2023-2024 ETH Zurich and Quantum Transport Toolbox authors. All rights reserved.
+
