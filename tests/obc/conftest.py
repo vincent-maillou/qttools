8b2b2cb945c6c398ccd968366ba986276c067db4
vetschn <vetschnicolas@gmail.com>
2024-04-26 17:53:01 +0200

    Refactor SBCOO class to optimize blockmap calculation in sbcoo.py

--- tests/obc/conftest.py
+++ tests/obc/conftest.py
@@ -8,7 +8,7 @@ ARRAY_SHAPE = (2, 2)
 @pytest.fixture(scope="package", autouse=True)
 def a_ii() -> np.ndarray:
     """Returns a random complex array."""
-    return 1e2 * (np.random.rand(*ARRAY_SHAPE) + 1j * np.random.rand(*ARRAY_SHAPE))
+    return np.random.rand(*ARRAY_SHAPE) + 1j * np.random.rand(*ARRAY_SHAPE)
 
 
 @pytest.fixture(scope="package", autouse=True)
