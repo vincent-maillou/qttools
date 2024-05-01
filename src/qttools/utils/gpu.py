1231be48f6d8cab58ded79f5938e932989054de6
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-05-01 15:30:43 +0200

    Add densify_block function for GPU in gpu.py and CPU in cpu.py

--- src/qttools/utils/gpu.py
+++ src/qttools/utils/gpu.py
@@ -1,1 +1,19 @@
-# Kernel to densify coo blocks
+import cupy as cp
+from cupy.scipy.sparse import spmatrix
+
+
+def densify_block(a: spmatrix) -> cp.ndarray:
+    """Densify a sparse block given on the GPU.
+
+    Parameters
+    ----------
+    a : spmatrix
+        Sparse matrix to densify. Stored on the GPU.
+
+    Returns
+    -------
+    cp.ndarray
+        Densified matrix. Stored on the GPU.
+    """
+
+    return a.todense()
