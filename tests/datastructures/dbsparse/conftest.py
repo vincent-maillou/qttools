808133053d3665d101ef790232d596b6c652ecd5
vetschn <vetschnicolas@gmail.com>
2024-05-14 16:42:38 +0200

    Fixed cursed fixture bug

--- tests/datastructures/dbsparse/conftest.py
+++ tests/datastructures/dbsparse/conftest.py
@@ -4,7 +4,7 @@ from scipy import sparse
 ARRAY_SHAPE = (100, 100)
 
 
-@pytest.fixture(scope="package", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def coo() -> sparse.coo_array:
     """Returns a random complex sparse array."""
     return sparse.random(*ARRAY_SHAPE, density=0.1, format="coo", dtype=complex)
--- tests/datastructures/dbsparse/test_creation.py
+++ tests/datastructures/dbsparse/test_creation.py
@@ -14,17 +14,17 @@ from qttools.datastructures.dbsparse import DBSparse
     ],
 )
 @pytest.mark.parametrize(
-    "stack_shape",
+    "global_stack_shape",
     [
         pytest.param((10,), id="1D-stack"),
-        pytest.param((10, 2), id="2D-stack"),
     ],
 )
 @pytest.mark.parametrize(
     "densify_blocks",
     [
         pytest.param(None, id="no-densify"),
-        pytest.param([(0, 0), (-1, -1)], id="densify"),
+        pytest.param([(0, 0), (-1, -1)], id="densify-boundary"),
+        pytest.param([(2, 4)], id="densify-random"),
     ],
 )
 @pytest.mark.parametrize("dbsparse_type", [DBCSR])
@@ -32,9 +32,13 @@ def test_from_sparray(
     coo: sparse.coo_array,
     dbsparse_type: DBSparse,
     block_sizes: np.ndarray,
-    stack_shape: int | tuple[int],
+    global_stack_shape: int | tuple[int],
     densify_blocks: list[tuple[int]] | None,
 ):
     """Tests that the from_sparray method works."""
-    dbsparse = dbsparse_type.from_sparray(coo, block_sizes, stack_shape, densify_blocks)
-    assert np.allclose(dbsparse.to_dense()[(0,) * len(stack_shape)], coo.toarray())
+    dbsparse = dbsparse_type.from_sparray(
+        coo, block_sizes, global_stack_shape, densify_blocks
+    )
+    assert np.allclose(
+        dbsparse.to_dense()[(0,) * len(global_stack_shape)], coo.toarray()
+    )
