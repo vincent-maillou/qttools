997f6203a364ca118a54807fd510bdcdb947cc64
Jiang Cao <jiang.cao89@gmail.com>
2024-11-13 22:53:35 +0100

    update

--- src/qttools/greens_function_solver/more_rgf.py
+++ src/qttools/greens_function_solver/more_rgf.py
@@ -1,11 +1,12 @@
 from qttools.utils.gpu_utils import xp
 
 
-def block_to_dense(A):
+def block_to_dense(A,block_size = None,dtype=xp.complex128):
     num_blocks = len(A)
-    block_size = A[0][0].shape[0]
+    if (block_size == None):
+        block_size = A[0][0].shape[0]
     tmp = xp.zeros(
-        (num_blocks * block_size, num_blocks * block_size), dtype=A[0][0].dtype
+        (num_blocks * block_size, num_blocks * block_size), dtype=dtype
     )
     for i in range(num_blocks):
         for j in range(num_blocks):
@@ -30,6 +31,9 @@ def random_block(block_size: int, num_blocks: int, num_offdiag: int = 1, complex
         if (symmetry == 'H'):
             a = (tmp[i][i] + tmp[i][i].conj().T) / 2 
             tmp[i][i] = a
+        elif (symmetry == 'AH'): 
+            a = (tmp[i][i] - tmp[i][i].conj().T) / 2
+            tmp[i][i] = a 
 
         for idiag in range(num_offdiag):
             if i < num_blocks - 1 - idiag:
@@ -41,6 +45,8 @@ def random_block(block_size: int, num_blocks: int, num_offdiag: int = 1, complex
 
                     if (symmetry == 'H'):
                         tmp[i + 1 + idiag][i] = ((tmp[i][i + 1 + idiag]).conj().T).copy()
+                    elif (symmetry == 'AH'):
+                        tmp[i + 1 + idiag][i] = - ((tmp[i][i + 1 + idiag]).conj().T).copy()
                     else:
                         tmp[i + 1 + idiag][i] = (
                             xp.random.random((block_size, block_size))
@@ -53,6 +59,8 @@ def random_block(block_size: int, num_blocks: int, num_offdiag: int = 1, complex
 
                     if (symmetry == 'H'):
                         tmp[i + 1 + idiag][i] = ((tmp[i][i + 1 + idiag]).T).copy()
+                    elif (symmetry == 'AH'):
+                        tmp[i + 1 + idiag][i] = - ((tmp[i][i + 1 + idiag]).T).copy()
                     else:
                         tmp[i + 1 + idiag][i] = (
                             xp.random.random((block_size, block_size))
@@ -75,12 +83,73 @@ def zeros_block(
                 tmp[i + idiag + 1][i] = xp.zeros((block_size, block_size), dtype=dtype)
     return tmp
 
+def block_matmul_single(alpha,A,B,block_sizes,dtype, i:int,j:int):    
+    tmp = xp.zeros((block_sizes[i],block_sizes[j]),dtype=dtype)
+    A_num_blocks = len(A)
+    for k in range(A_num_blocks):
+        if ( ((A[i][k] is not None) and ((B[k][j] is not None))) ):
+            tmp += alpha * A[i][k] @ B[k][j]
+    # if the block is not empty then return dense block, otherwise return None        
+    if ( not( (xp.abs(tmp) < 1e-30).all() ) ):
+        return tmp
+    else: 
+        return None
 
-def selected_inv(a_, num_offdiag: int = 1):
+def block_matmul(alpha,A,B,C,block_sizes,dtype):    
+    C_num_blocks = len(C)
+    for i in range(C_num_blocks):
+        for j in range(C_num_blocks):            
+            C[i][j] = block_matmul_single(alpha,A,B,block_sizes,dtype,i=i,j=j)
+
+def duplicate_block_matrix(A):
+    num_blocks = len(A)
+    B = [[None for j in range(num_blocks)] for i in range(num_blocks)]
+    for i in range(num_blocks):
+        for j in range(num_blocks):
+            if (A[i][j] is not None):
+                B[i][j] = A[i][j].copy()
+    return B
+
+def assign_block_matrix(A,B):
+    num_blocks = len(A)
+    for i in range(num_blocks):
+        for j in range(num_blocks):
+            if (A[i][j] is not None):
+                B[i][j] = xp.zeros_like(A[i][j])                
+                B[i][j] += A[i][j]
+
+def block_norm(A):
+    num_blocks = len(A)
+    normA = 0
+    for i in range(num_blocks):
+        for j in range(num_blocks):
+            if (A[i][j] is not None):
+                normA += xp.sum(xp.abs(A[i][j]))
+    return normA   
+
+def block_add(A,B,alpha,beta):
+    # alpha * A + beta * B -> A
+    aii_blocksize = len(A)
+    for ii in range(aii_blocksize):
+        for jj in range(aii_blocksize):
+            if ( (not(A[ii][jj] is None)) ):
+                A[ii][jj] *= alpha
+                if (not(B[ii][jj] is None)):
+                    A[ii][jj] += B[ii][jj] * beta
+            else:
+                if (not(B[ii][jj] is None)):
+                    A[ii][jj] = xp.zeros_like(B[ii][jj])
+                    A[ii][jj] += B[ii][jj] * beta
+
+def selected_inv(a_, num_offdiag: int = 1, out = None):
     num_blocks = len(a_)
     block_size = a_[0][0].shape[0]
 
-    x_ = zeros_block(block_size, num_blocks, num_offdiag, dtype=a_[0][0].dtype)
+    if (out is not None):
+        x_ = out
+    else:
+        x_ = zeros_block(block_size, num_blocks, num_offdiag, dtype=a_[0][0].dtype)
+    
     x_[0][0] = xp.linalg.inv(a_[0][0])
 
     # Forwards sweep.
@@ -94,25 +163,27 @@ def selected_inv(a_, num_offdiag: int = 1):
 
     # Backwards sweep.
     for i in range(num_blocks - 2, -1, -1):
-
         for j in range(i + 1, num_blocks):
-            if (a_[j][i] is not None) and (a_[i][j] is not None):
-                for k in range(i + 1, num_blocks):
-                    if (
-                        (x_[k][j] is not None)
-                        and (x_[j][k] is not None)
-                        and (x_[k][i] is not None)
-                        and (x_[i][k] is not None)
-                    ):
-                        x_[k][i] -= x_[k][j] @ a_[j][i] @ x_[i][i]
-                        x_[i][k] -= x_[i][i] @ a_[i][j] @ x_[j][k]
+            for k in range(i + 1, num_blocks):
+                if ((a_[j][i] is not None) 
+                    and (x_[k][j] is not None)
+                    and (x_[k][i] is not None)
+                ):
+                    x_[k][i] -= x_[k][j] @ a_[j][i] @ x_[i][i]
+                if ((a_[i][j] is not None)
+                    and (x_[j][k] is not None)
+                    and (x_[i][k] is not None)
+                ):
+                    x_[i][k] -= x_[i][i] @ a_[i][j] @ x_[j][k]
 
         tmp = xp.zeros_like(x_[i][i])
         for j in range(i + 1, num_blocks):
             if (x_[j][i] is not None) and (a_[i][j] is not None):
                 tmp -= x_[i][i] @ a_[i][j] @ x_[j][i]
         x_[i][i] += tmp
-    return x_
+    
+    if (out is None):
+        return x_
 
 
 def test_selected_inv(num_blocks, block_size, num_offdiag):
@@ -139,3 +210,68 @@ def test_selected_inv(num_blocks, block_size, num_offdiag):
             print(i, j, xp.allclose(x[i][j], g00))
             g00 = get_block(Gfull, block_size, j, i)
             print(j, i, xp.allclose(x[j][i], g00))
+
+
+def sancho(a_ii, a_ij, a_ji,block_sizes, max_iterations, convergence_tol, 
+           alpha = None, beta = None, epsilon = None, epsilon_s = None, GBB = None):
+        num_blocks = len(a_ii)
+        m = [[None for j in range(num_blocks)] for i in range(num_blocks)]
+        if (epsilon is None):      
+            epsilon = duplicate_block_matrix(a_ii)
+        if (epsilon_s is None):
+            epsilon_s = duplicate_block_matrix(a_ii)
+        if (alpha is None):
+            alpha = duplicate_block_matrix(a_ji)
+        if (beta is None):
+            beta = duplicate_block_matrix(a_ij)   
+
+        C = [[None for j in range(num_blocks)] for i in range(num_blocks)]
+        D = [[None for j in range(num_blocks)] for i in range(num_blocks)]
+        dtype = xp.complex128
+        num_offdiag = num_blocks - 1
+        delta = float("inf")
+        for iter in range(max_iterations):
+            
+            assign_block_matrix(epsilon,m)
+            inverse = selected_inv(m,num_offdiag=num_offdiag)         
+
+            block_matmul(1,alpha,inverse,C,block_sizes,dtype)
+            block_matmul(1,C,beta,D,block_sizes,dtype)
+            
+            block_add(epsilon,D,1,-1)
+            block_add(epsilon_s,D,1,-1)
+
+            block_matmul(1,C,alpha,D,block_sizes,dtype)
+            
+            block_matmul(1,beta,inverse,C,block_sizes,dtype)
+            block_matmul(1,C,alpha,D,block_sizes,dtype)            
+            block_add(epsilon,D,1,-1)
+
+            block_matmul(1,C,beta,D,block_sizes,dtype)
+            assign_block_matrix(D,beta)
+
+            block_matmul(1,alpha,inverse,C,block_sizes,dtype)
+            block_matmul(1,C,alpha,D,block_sizes,dtype)
+            assign_block_matrix(D,alpha)
+
+            # epsilon = epsilon - alpha @ inverse @ beta - beta @ inverse @ alpha
+            # epsilon_s = epsilon_s - alpha @ inverse @ beta
+
+            # alpha = alpha @ inverse @ alpha
+            # beta = beta @ inverse @ beta            
+
+            delta = (block_norm(alpha) + block_norm(beta)) / 2
+
+            if delta < convergence_tol:
+                break
+
+        else:  # Did not break, i.e. max_iterations reached.
+            raise RuntimeError("Surface Green's function did not converge.")
+
+        x_ii = selected_inv(epsilon_s,num_offdiag=num_offdiag)
+        # print('sancho converges in ', iter)
+        if (GBB is not None):
+            inverse = selected_inv(epsilon,num_offdiag=num_offdiag)
+            GBB = inverse
+
+        return x_ii
