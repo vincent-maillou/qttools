e3e26b745ab89a83c3ba9abeeb8fe598f0e98dea
vetschn <vetschnicolas@gmail.com>
2024-11-17 22:04:12 +0100

    Refactored block_index computation

--- src/qttools/kernels/cuda/dsbsparse.py
+++ src/qttools/kernels/cuda/dsbsparse.py
@@ -3,7 +3,6 @@ from cupy.typing import ArrayLike
 from cupyx import jit
 
 from qttools.kernels.cuda import THREADS_PER_BLOCK
-from qttools.kernels.cuda.dsbcoo import _compute_coo_block_mask_kernel
 
 
 @jit.rawkernel()
@@ -54,63 +53,3 @@ def find_ranks(nnz_section_offsets: ArrayLike, inds: ArrayLike) -> ArrayLike:
         (nnz_section_offsets, inds, ranks),
     )
     return ranks
-
-
-def compute_block_sort_index(
-    coo_rows: ArrayLike, coo_cols: ArrayLike, block_sizes: ArrayLike
-) -> ArrayLike:
-    """Computes the block-sorting index for a sparse matrix.
-
-    Note
-    ----
-    Due to the Python for loop around the kernel, this method will
-    perform best for larger block sizes (>500).
-
-
-    Parameters
-    ----------
-    coo_rows : array_like
-        The row indices of the matrix in coordinate format.
-    coo_cols : array_like
-        The column indices of the matrix in coordinate format.
-    block_sizes : array_like
-        The block sizes of the block-sparse matrix we want to construct.
-
-    Returns
-    -------
-    sort_index : array_like
-        The indexing that sorts the data by block-row and -column.
-
-    """
-    num_blocks = block_sizes.shape[0]
-    block_offsets = cp.hstack((cp.array([0]), cp.cumsum(block_sizes)))
-
-    sort_index = cp.zeros(len(coo_cols), dtype=cp.int32)
-    mask = cp.zeros(len(coo_cols), dtype=cp.bool_)
-
-    blocks_per_grid = (len(coo_cols) + THREADS_PER_BLOCK - 1) // THREADS_PER_BLOCK
-    offset = 0
-    for i, j in cp.ndindex(num_blocks, num_blocks):
-        _compute_coo_block_mask_kernel(
-            (blocks_per_grid,),
-            (THREADS_PER_BLOCK,),
-            (
-                coo_rows,
-                coo_cols,
-                int(block_offsets[i]),
-                int(block_offsets[i + 1]),
-                int(block_offsets[j]),
-                int(block_offsets[j + 1]),
-                mask,
-            ),
-        )
-
-        bnnz = cp.sum(mask)
-
-        if bnnz != 0:
-            # Sort the data by block-row and -column.
-            sort_index[offset : offset + bnnz] = cp.nonzero(mask)[0]
-
-            offset += bnnz
-
-    return sort_index
