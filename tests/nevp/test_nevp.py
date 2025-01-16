09cbb483076764362f21e159c80b9a2fd90d3f81
almaeder <alexander.maeder@gmx.ch>
2025-01-16 18:00:18 +0100

    Both left and right eigenvectors computations.
    Enables solving for both left and right eigenvectors with the nevp solver,
    adapts the spectral OBC calculation to use them, and extends the tests accordingly.

--- tests/nevp/test_nevp.py
+++ tests/nevp/test_nevp.py
@@ -7,31 +7,54 @@ from qttools import NDArray, xp
 from qttools.nevp import Beyn, Full
 
 
-def test_full(a_xx: tuple[NDArray, ...]):
+@pytest.mark.usefixtures("left")
+def test_full(a_xx: tuple[NDArray, ...], left: bool):
     """Tests that the Full NEVP solver returns the correct result."""
     full_nevp = Full()
-    ws, vs = full_nevp(a_xx)
+    if left:
+        wrs, vrs, wls, vls = full_nevp(a_xx, left=left)
+    else:
+        wrs, vrs = full_nevp(a_xx, left=left)
+
     a_ji, a_ii, a_ij = a_xx
 
-    for i in range(ws.shape[1]):
-        w = ws[0, i]
-        v = vs[0, :, i] / xp.linalg.norm(vs[0, :, i])
+    for i in range(wrs.shape[1]):
+        w = wrs[0, i]
+        v = vrs[0, :, i] / xp.linalg.norm(vrs[0, :, i])
 
         assert xp.allclose((a_ji / w + a_ii + a_ij * w) @ v, 0)
 
+    if left:
+        for i in range(wrs.shape[1]):
+            w = wls[0, i]
+            v = vls[0, :, i] / xp.linalg.norm(vls[0, :, i])
 
-@pytest.mark.usefixtures("subspace_nevp")
-def test_subspace(a_xx: tuple[NDArray, ...], subspace_nevp: Beyn):
+            assert xp.allclose(v.conj().T @ (a_ji / w + a_ii + a_ij * w), 0)
+
+
+@pytest.mark.usefixtures("subspace_nevp", "left")
+def test_subspace(a_xx: tuple[NDArray, ...], subspace_nevp: Beyn, left: bool):
     """Tests that the subspace NEVP solver returns the correct result."""
-    ws, vs = subspace_nevp(a_xx)
+    if left:
+        wrs, vrs, wls, vls = subspace_nevp(a_xx, left=left)
+    else:
+        wrs, vrs = subspace_nevp(a_xx, left=left)
 
     a_ji, a_ii, a_ij = a_xx
     residuals = []
-    for k in range(ws.shape[1]):
-        w = ws[0, k]
-        v = vs[0, :, k] / xp.linalg.norm(vs[0, :, k])
+    for k in range(wrs.shape[1]):
+        w = wrs[0, k]
+        v = vrs[0, :, k] / xp.linalg.norm(vrs[0, :, k])
         with np.errstate(divide="ignore", invalid="ignore"):
             residuals.append(xp.linalg.norm((a_ji / w + a_ii + a_ij * w) @ v))
+    if left:
+        for k in range(wrs.shape[1]):
+            w = wls[0, k]
+            v = vls[0, :, k] / xp.linalg.norm(vls[0, :, k])
+            with np.errstate(divide="ignore", invalid="ignore"):
+                residuals.append(
+                    xp.linalg.norm(v.conj().T @ (a_ji / w + a_ii + a_ij * w))
+                )
 
     residuals = xp.nan_to_num(xp.array(residuals))
 
