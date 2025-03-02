6f0657215ea07a62538d67cab25989348ddbb7a4
almaeder <alexander.maeder@gmx.ch>
2025-03-02 15:32:45 +0100

    Numba parallelized SVD

--- tests/kernels/test_svd.py
+++ tests/kernels/test_svd.py
@@ -2,11 +2,41 @@ import numpy as np
 import pytest
 
 from qttools import xp
-from qttools.kernels.svd import svd
+from qttools.kernels.svd import _svd_numba_ndarray, svd
 from qttools.utils.gpu_utils import get_host
 
 if xp.__name__ == "cupy":
     import cupy as cp
+
+
+@pytest.mark.usefixtures("m", "n", "full_matrices", "batch_shape")
+def test_svd_numba_ndarray(
+    m: int, n: int, full_matrices: bool, batch_shape: tuple[int, ...]
+):
+    """Tests the _svd_numba_ndarray function."""
+
+    rng = np.random.default_rng()
+
+    batch_size = np.prod(batch_shape)
+
+    A = rng.random((batch_size, m, n)) + 1j * rng.random((batch_size, m, n))
+
+    u, s, vh = _svd_numba_ndarray(
+        A,
+        full_matrices=full_matrices,
+    )
+
+    k = min(m, n)
+    if full_matrices:
+        u = u[:, :, :k]
+        s = s[:, :k]
+        vh = vh[:, :k, :]
+
+    s_dense = np.zeros((batch_size, k, k), dtype=A.dtype)
+    for i in range(batch_size):
+        s_dense[i] = np.diag(s[i])
+
+    assert xp.allclose(A, u @ s_dense @ vh)
 
 
 @pytest.mark.usefixtures(
@@ -54,3 +84,71 @@ def test_svd(
         vh = vh[:k, :]
 
     assert xp.allclose(A, u @ np.diag(s) @ vh)
+
+
+@pytest.mark.usefixtures(
+    "m",
+    "n",
+    "batch_shape",
+    "full_matrices",
+    "compute_module",
+    "input_module",
+    "output_module",
+)
+def test_svd_batched(
+    m: int,
+    n: int,
+    batch_shape: tuple[int, ...],
+    full_matrices: bool,
+    compute_module: str,
+    input_module: str,
+    output_module: str,
+):
+    """Tests the svd function."""
+
+    if xp.__name__ == "numpy" and (
+        compute_module == "cupy" or output_module == "cupy" or input_module == "cupy"
+    ):
+        return
+
+    if input_module == "cupy":
+        rng = cp.random.default_rng()
+    elif input_module == "numpy":
+        rng = np.random.default_rng()
+
+    A = rng.random((*batch_shape, m, n)) + 1j * rng.random((*batch_shape, m, n))
+
+    u, s, vh = svd(
+        A,
+        compute_module=compute_module,
+        output_module=output_module,
+        full_matrices=full_matrices,
+    )
+
+    # check residual on the host
+    u = get_host(u)
+    s = get_host(s)
+    vh = get_host(vh)
+    A = get_host(A)
+
+    k = min(m, n)
+    if full_matrices:
+        u = u.reshape((-1, m, m))
+        vh = vh.reshape((-1, n, n))
+    else:
+        u = u.reshape((-1, m, k))
+        vh = vh.reshape((-1, k, n))
+    s = s.reshape((-1, k))
+    A = A.reshape((-1, m, n))
+
+    if full_matrices:
+        u = u[:, :, :k]
+        s = s[:, :k]
+        vh = vh[:, :k, :]
+
+    batch_size = np.prod(batch_shape)
+    s_dense = np.zeros((batch_size, k, k), dtype=A.dtype)
+    for i in range(batch_size):
+        s_dense[i] = np.diag(s[i])
+
+    assert xp.allclose(A, u @ s_dense @ vh)
