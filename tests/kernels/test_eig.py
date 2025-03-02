a2282f01a5c58790747b9558940d20cdc85984d3
almaeder <alexander.maeder@gmx.ch>
2025-03-02 14:43:22 +0100

    Restructuring of beyn to allow numba parallel eig with non uniform sizes.
    Implementing of numba parallel eig with list input.
    Extension of eig tests.

--- tests/kernels/test_eig.py
+++ tests/kernels/test_eig.py
@@ -1,16 +1,68 @@
 import numpy as np
 import pytest
+from numba.typed import List
 
 from qttools import xp
-from qttools.kernels.eig import eig
+from qttools.kernels.eig import _eig_numba_list, _eig_numba_ndarray, eig
 from qttools.utils.gpu_utils import get_host
 
 if xp.__name__ == "cupy":
     import cupy as cp
 
 
-@pytest.mark.usefixtures("n", "compute_module", "input_module", "output_module")
-def test_eig(n: int, compute_module: str, input_module: str, output_module: str):
+@pytest.mark.usefixtures("n", "batch_shape")
+def test_eig_numba_ndarray(n: int, batch_shape: tuple[int, ...]):
+    rng = np.random.default_rng()
+
+    batch_size = np.prod(batch_shape)
+
+    A = rng.random((batch_size, n, n)) + 1j * rng.random((batch_size, n, n))
+
+    w, v = _eig_numba_ndarray(A)
+
+    for b in range(batch_size):
+        for i in range(n):
+            assert xp.allclose(A[b] @ v[b][:, i], w[b][i] * v[b][:, i])
+
+
+@pytest.mark.usefixtures("n", "batch_shape")
+def test_eig_numba_list(n: int, batch_shape: tuple[int, ...]):
+    rng = np.random.default_rng()
+
+    batch_size = np.prod(batch_shape)
+
+    A = []
+    for _ in range(batch_size):
+        A.append(rng.random((n, n)) + 1j * rng.random((n, n)))
+    for _ in range(batch_size):
+        A.append(rng.random((n // 2, n // 2)) + 1j * rng.random((n // 2, n // 2)))
+    for _ in range(batch_size // 2):
+        A.append(rng.random((n // 3, n // 3)) + 1j * rng.random((n // 3, n // 3)))
+
+    batch_size = len(A)
+
+    A = List(A)
+    w = List([np.empty((a.shape[-1]), dtype=a.dtype) for a in A])
+    v = List([np.empty((a.shape[-1], a.shape[-1]), dtype=a.dtype) for a in A])
+
+    _eig_numba_list(A, w, v)
+
+    for b in range(batch_size):
+        n_ = A[b].shape[-1]
+        for i in range(n_):
+            assert xp.allclose(A[b] @ v[b][:, i], w[b][i] * v[b][:, i])
+
+
+@pytest.mark.usefixtures(
+    "n", "compute_module", "input_module", "output_module", "if_list"
+)
+def test_eig(
+    n: int,
+    compute_module: str,
+    input_module: str,
+    output_module: str,
+    if_list: bool,
+):
     """Tests the eig function."""
 
     if xp.__name__ == "numpy" and (
@@ -27,7 +79,15 @@ def test_eig(n: int, compute_module: str, input_module: str, output_module: str)
 
     A = rng.random((n, n)) + 1j * rng.random((n, n))
 
+    if if_list:
+        A = [A]
+
     w, v = eig(A, compute_module=compute_module, output_module=output_module)
+
+    if if_list:
+        w = w[0]
+        v = v[0]
+        A = A[0]
 
     # check residual on the host
     w = get_host(w)
@@ -39,7 +99,7 @@ def test_eig(n: int, compute_module: str, input_module: str, output_module: str)
 
 
 @pytest.mark.usefixtures(
-    "n", "batch_shape", "compute_module", "input_module", "output_module"
+    "n", "batch_shape", "compute_module", "input_module", "output_module", "if_list"
 )
 def test_eig_batched(
     n: int,
@@ -47,6 +107,7 @@ def test_eig_batched(
     compute_module: str,
     input_module: str,
     output_module: str,
+    if_list: bool,
 ):
     """Tests the eig function."""
 
@@ -57,6 +118,9 @@ def test_eig_batched(
     if compute_module == "cupy" and (hasattr(xp.linalg, "eig") is False):
         return
 
+    if if_list and len(batch_shape) > 1:
+        return
+
     if input_module == "cupy":
         rng = cp.random.default_rng()
     elif input_module == "numpy":
@@ -64,7 +128,15 @@ def test_eig_batched(
 
     A = rng.random((*batch_shape, n, n)) + 1j * rng.random((*batch_shape, n, n))
 
+    if if_list:
+        A = [a for a in A]
+
     w, v = eig(A, compute_module=compute_module, output_module=output_module)
+
+    if if_list:
+        A = xp.array([a for a in A])
+        w = xp.array([w_ for w_ in w])
+        v = xp.array([v_ for v_ in v])
 
     assert w.shape[:-1] == batch_shape
     assert v.shape[:-2] == batch_shape
