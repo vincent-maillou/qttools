54a519efe70cbf7c44397678dfe04001296b2270
almaeder <alexander.maeder@gmx.ch>
2025-03-15 20:18:30 +0100

    Removed lyapunov vectorized support

--- src/qttools/lyapunov/__init__.py
+++ src/qttools/lyapunov/__init__.py
@@ -3,6 +3,5 @@
 from qttools.lyapunov.doubling import Doubling
 from qttools.lyapunov.lyapunov import LyapunovMemoizer, LyapunovSolver
 from qttools.lyapunov.spectral import Spectral
-from qttools.lyapunov.vectorize import Vectorize
 
-__all__ = ["LyapunovSolver", "LyapunovMemoizer", "Doubling", "Spectral", "Vectorize"]
+__all__ = ["LyapunovSolver", "LyapunovMemoizer", "Doubling", "Spectral"]
