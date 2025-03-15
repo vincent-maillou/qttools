082b62bdbcd8401025fb9e6dfb5ea9133e728c5f
Alexandros Nikolaos Ziogas <alexandros.ziogas@iis.ee.ethz.ch>
2025-03-15 12:02:16 +0100

    Added linalg.inv method that switched between inv and solve (AMD/NVIDIA).

--- src/qttools/kernels/linalg/__init__.py
+++ src/qttools/kernels/linalg/__init__.py
@@ -3,11 +3,13 @@
 
 from qttools.kernels.linalg.eig import eig
 from qttools.kernels.linalg.eigvalsh import eigvalsh
+from qttools.kernels.linalg.inv import inv
 from qttools.kernels.linalg.qr import qr
 from qttools.kernels.linalg.svd import svd
 
 __all__ = [
     "eig",
+    "inv",
     "svd",
     "eigvalsh",
     "qr",
