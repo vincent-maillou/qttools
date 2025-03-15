75404d30beb89828dc76ff4d2a140d23e202cd47
almaeder <alexander.maeder@gmx.ch>
2025-03-15 14:38:44 +0100

    removed comment

--- src/qttools/datastructures/dsbcoo.py
+++ src/qttools/datastructures/dsbcoo.py
@@ -508,7 +508,6 @@ class DSBCOO(DSBSparse):
             data = self.data.reshape(-1, self.data.shape[-1])
             for stack_idx in range(data.shape[0]):
                 data[stack_idx] = data[stack_idx, inds_bcoo2bcoo]
-            # self.data[:] = self.data[..., inds_bcoo2bcoo]
             self.rows = self.rows[inds_bcoo2bcoo]
             self.cols = self.cols[inds_bcoo2bcoo]
 
@@ -669,9 +668,6 @@ class DSBCOO(DSBSparse):
             data[stack_idx] = 0.5 * op(
                 data[stack_idx], data[stack_idx, self._inds_bcoo2bcoo_t].conj()
             )
-        # self.data[:] = 0.5 * op(
-        #     self.data, self.data[..., self._inds_bcoo2bcoo_t].conj()
-        # )
 
     @profiler.profile(level="api")
     def spy(self) -> tuple[NDArray, NDArray]:
